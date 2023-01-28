dfa48f4936b6b08572758eadebca96a2ec249804
Duy Vũ <vqduydz@gmail.com>
2023-01-28 22:09:18 +0700

    #1 set up

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,17 @@
-const getHomePage = (req, res) => {
-  return res.render("homepage.ejs");
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    console.log(data);
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
+
 const getUsersApi = (req, res) => {
   return res.render("homepage.ejs");
 };
